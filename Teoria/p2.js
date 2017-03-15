e99f6dd1d50c4d28a444a2265f128d48bbfb8d18
André Michel Andy Pozos <andreandyp@outlook.com>
2017-03-15 09:25:46 -0600

    Empezando con proyecto de Bases

--- Teoria/p2.js
+++ Teoria/p2.js
@@ -1,8 +1,18 @@
 function validarExpreg() {
-	var expreg = /^[A-Z](A|E|I|O|U)[A-Z][A-Z][0-9][0-9](1[0-2]|0[0-9])([0-2][0-9]|3[0-1])(H|M)([A-D]|G|H|J|[M-Q]|S|T|V|Y|Z)(C|[F-H]|[L-N]|P|[R-T]|X|Z)[A-Z][A-Z][A-Z]([0-9][0-9])?$/
+	var expreg = /^[A-Z](A|E|I|O|U)[A-Z][A-Z][0-9][0-9](1[0-2]|0[0-9])([0-2][0-9]|3[0-1])(H|M)([A-D]|G|H|J|[M-Q]|S|T|V|Y|Z)(C|[F-H]|[L-N]|P|[R-T]|X|Z)[A-Z][A-Z][A-Z]([0-9][0-9])$/;
 	if(expreg.test(arguments[0].toUpperCase()) === false)
-		console.log("No coincide con la expreg");
+		resultados(arguments[0],false);
 	else
-		console.log("Si coincide con la expreg");
+		resultados(arguments[0],true);
 }
-validarExpreg("SAGL770731MDFMRZ000");
+function resultados() {
+	var texto = document.createElement("p");
+	if(arguments[1] === true){
+		texto.appendChild(document.createTextNode("La cadena "+arguments[0]+" sí coincide con la expresión regular"));
+	}
+	else{
+		texto.setAttribute("style","color: red;");
+		texto.appendChild(document.createTextNode("La cadena "+arguments[0]+" no coincide con la expresión regular"));	
+	}
+	document.getElementById('resultados').appendChild(texto);
+}
